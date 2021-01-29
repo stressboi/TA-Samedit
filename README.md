505995a94e7b57f65a0b8e65d89a145491de6605
FROSTBITE\james <james.brodsky@gmail.com>
2021-01-28 18:52:44 -0700

    V2: Detection method moved from sudoedit to simple version checking of sudo.

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # TA-Samedit
 
-Simple Splunk UF detection for Baron Samedit sudo buffer overflow (CVE-2021-3156).
+Simple Splunk UF detection for Baron Samedit sudo buffer overflow (CVE-2021-3156). Refer to https://blog.qualys.com/vulnerabilities-research/2021/01/26/cve-2021-3156-heap-based-buffer-overflow-in-sudo-baron-samedit.
 
-This takes the detection method from https://blog.qualys.com/vulnerabilities-research/2021/01/26/cve-2021-3156-heap-based-buffer-overflow-in-sudo-baron-samedit and converts it into a scripted input for any Splunk Universal Forwarder running on a Linux server or endpoint. This detection method is not foolproof but parses the output of stderror from a sudoedit command. If that output contains "sudoedit:" then the version of sudo is vulnerable.
+1-28-2021: V2: Redid detection to not rely on sudoedit command. V1 (found in bin directory) required UF to run as root! No bueno.
+
+This scripted input captures the version of sudo found on the system and compares to a known-good list of sudo versions. It also takes into account Ubuntu systems that even after patching may still show a sudo --version known to be vulnerable and may theefore result in a false positive detection. This can be used on any Splunk Universal Forwarder running on a Linux server or endpoint. This detection method is not foolproof but parses the output of either sudo --version or apt list sudo. The version is then compared against a hard-coded list in the script.
 
 Simply drop it into the /etc/apps directory on a Universal Forwarder (or use deployment server or your favorite distribution method to get it there). By default it runs once an hour and outputs key/value pairs "samedit_status" and "finding" to tell you if that particular Linux host is potentially vulnerable. It also outputs the sudo version found.
 
--- bin/samedit.sh
+++ bin/samedit.sh
@@ -2,9 +2,9 @@
 # Baron Samedit checker 
 # https://www.sudo.ws/alerts/unescape_overflow.html
 # brodsky@splunk.com 
-# 26 JAN 2021
+# 28 JAN 2021
 
-VERSION=0.1-SplunkUF
+VERSION=0.2-SplunkUF
 
 # set a date format so we can output a date for each line
 DATETIME=$(date '+%Y-%m-%d %H:%M:%S %Z')
@@ -13,24 +13,40 @@ DATETIME=$(date '+%Y-%m-%d %H:%M:%S %Z')
 IPADD=$(hostname -I)
 
 # check for sudo
-if ! command -v sudo &> /dev/null
+if ! command -v sudo > /dev/null
 then
 	echo "$DATETIME src_ip=$IPADD finding='no sudo on this machine or in path'" 
 	exit
 fi
 
-# what version is sudo?
-SUDOVERSION=$(sudo -V |grep "Sudo version" | cut -c14-50)
+# any linux: what version is sudo?
+VERSION=$(sudo -V |grep "Sudo version" | cut -c14-50)
 
-# run sudo and parse output
-sudoedit -s / 2> sudoout
-SUDOOUT=$(<sudoout)
-rm sudoout
-
-if [[ $SUDOOUT =~ "sudoedit" ]];
+# check for ubuntu, if ubuntu then use apt to find version
+if command -v lsb_release > /dev/null
 then
-	echo "$DATETIME src_ip=$IPADD finding='CVE-2021-3156 found. Patch this system.' samedit_status=1 sudo_v='$SUDOVERSION'"
-else
-	echo "$DATETIME src_ip=$IPADD finding='CVE-2021-3156 NOT found.' samedit_status=0 sudo_v='$SUDOVERSION'"
+	UBUNTUVERSION=$(apt list --installed 2> /dev/null |grep sudo| cut -d ' ' -f 2)
 fi
 
+# if ubuntu, reset sudo version found to apt output
+if [ -z ${UBUNTUVERSION+x} ]; then SUDOVERSION=$VERSION; else SUDOVERSION=$UBUNTUVERSION; fi
+
+# is this version vuln
+case $SUDOVERSION in
+	1.9.5p2) VULNSTATUS=OK ;;
+	1.9.4p2-2ubuntu2) VULNSTATUS=OK ;;
+	1.9.1-1ubuntu1.1) VULNSTATUS=OK ;;
+	1.8.31-1ubuntu1.2) VULNSTATUS=OK ;;
+	1.8.21p2-3ubuntu1.4) VULNSTATUS=OK ;;
+	1.8.16-0ubuntu1.10) VULNSTATUS=OK ;;
+	1.8.9p5-1ubuntu1.5+esm6) VULNSTATUS=OK ;;
+	*) VULNSTATUS=PATCH ;;
+esac
+
+case $VULNSTATUS in
+	OK) STATUS=0 ;;
+	*) STATUS=1 ;;
+esac
+
+# output finding
+echo "$DATETIME src_ip=$IPADD finding='CVE-2021-3156 status is $VULNSTATUS' samedit_status=$STATUS sudo_v='$SUDOVERSION'"
